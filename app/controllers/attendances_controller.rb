6a5761e027685cda82c14fb2a94e4676e098bdd2
nico-shimo <kunikuni551981@gmail.com>
2020-12-21 17:50:45 +0900

    出欠の値の保存

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,20 @@
 class AttendancesController < ApplicationController
   def new
+    @attendance = Attendance.new
+  end
+
+  def create
+    @attendance = Attendance.new(attendance_params)
+    if @attendance.valid?
+      @attendance.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def attendance_params
+    params.require(:attendance).permit(:presence_id, :breakfast_id, :bath_id, :temperture_id).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'top/index'
   root to: "top#index"
-  resources :attendances, only: :new
+  resources :attendances, only: [:new, :create]
 end
