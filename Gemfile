9f70d5b5be360a2a4f15c6e334fef9118085a214
nico-shimo <kunikuni551981@gmail.com>
2020-12-23 18:35:59 +0900

    お知らせ投稿フォーム作成（画像付き）

--- Gemfile
+++ Gemfile
@@ -59,4 +59,6 @@ gem 'devise'
 
 gem 'active_hash'
 
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
 
--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -1,2 +1,13 @@
 class PrintsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  private
+
+  def print_params
+    params.require(:print).permit(:title, :text, :image).merge(user_id: current_user.id)
+  end
 end
