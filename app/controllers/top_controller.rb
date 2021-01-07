0b3256f1d364236a9f485f127296368d3dc531de
nico-shimo <kunikuni551981@gmail.com>
2021-01-08 00:00:55 +0900

    ゲストログイン機能実装

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,15 @@
 class TopController < ApplicationController
   def index
   end
+
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guest"
+      user.child_name = "rukia"
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
 end
