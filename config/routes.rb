1f4bee146c302cd5247c0ebdd1887229c894477c
nico-shimo <kunikuni551981@gmail.com>
2021-01-21 21:48:40 +0900

    メッセージの表示

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root to: "top#index"
   resources :attendances, only: [:index, :new, :create]
   resources :prints, only: [:index, :new, :create]
-  resources :messages, only: :index 
-  resources :rooms, only: [:index, :new, :create]
-  
+  resources :rooms, only: [:index, :new, :create] do
+    resources :messages, only:[:index, :create]
+  end
 end
