cacc3651fdc12e37de89f66f34bcbceb023faf14
nico-shimo <kunikuni551981@gmail.com>
2021-01-21 13:00:35 +0900

    roomの並び替え

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
   has_many :room_users
   has_many :users, through: :room_users
-
+  has_many :messages
   validates :name, presence: true
 end
