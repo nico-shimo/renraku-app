93d7d221dde6314d75b2dd6b2716533038ae8767
nico-shimo <kunikuni551981@gmail.com>
2021-05-09 06:19:48 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   resources :rooms, only: [:index, :new, :create, :destroy] do
     resources :messages, only:[:index, :create]
   end
+  resources :users, only: :show
 end
